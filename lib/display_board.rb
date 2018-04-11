886f57849dc4b48c7c18a04fcc9e7e3019afd6b5
Dennis Lord <lorddennis1006@gmail.com>
2018-04-11 15:57:08 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,21 +1,3 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
- # puts <<-DOC	
- #    |   |
- # -----------
- #    |   |
- # ------------
- #    |   |
- # DOC
- puts "   |   |
-------------
-   |   |
-------------
-  |   |  "
-
-  # puts "   |   |   "
-  # puts "-----------"
-  # puts "   |   |   "
-  # puts "-----------"
-  # puts "   |   |   "
--end
+end
